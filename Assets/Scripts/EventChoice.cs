b68307e207ac98b2545ad87e39c266fd3f66fdd3
Antho <transkuja@hotmail.fr>
2020-02-01 14:52:07 +0100

    Commit progress on event choice/data

--- Assets/Scripts/EventChoice.cs
+++ Assets/Scripts/EventChoice.cs
@@ -19,19 +19,51 @@ public class EventChoice
         public int charactersCost;
         public int toolsCost;
 
+        public bool lethalForRessources;
         public List<string> namedCharacters;
         public List<string> namedTools;
 
         public void ResolveCosts()
         {
             PlayerData.timeLeft -= timeCost;
+            CheckRessources(namedCharacters, true);
+            CheckRessources(namedTools, false);
 
+            NotNamedCosts(PlayerData.characters, true);
+            NotNamedCosts(PlayerData.tools, false);
+        }
 
+        void CheckRessources(List<string> _ids, bool _isCharacter)
+        {
+            foreach (var id in _ids)
+            {
+                PlayerData.DamageRessource(id, _isCharacter, lethalForRessources);
+            }
+        }
+
+        void NotNamedCosts(List<GameplayRessource> _playerData, bool _isCharacter)
+        {
+            List<GameplayRessource> gameplayRessourcesNotNamed = _playerData.FindAll(x => string.IsNullOrEmpty(x.ressourceName));
+            int diff = gameplayRessourcesNotNamed.Count - ((_isCharacter) ? charactersCost : toolsCost);
+            for (int i = 0; i < ((_isCharacter) ? charactersCost : toolsCost) && i < gameplayRessourcesNotNamed.Count; i++)
+            {
+                _playerData.Remove(gameplayRessourcesNotNamed[i]);
+            }
+
+            if (diff < 0)
+            {
+                PlayerData.ShuffleRessources();
+                List<GameplayRessource> gameplayRessourcesNamed = _playerData.FindAll(x => !string.IsNullOrEmpty(x.ressourceName));
+                for (int i = 0; i < diff && i < gameplayRessourcesNamed.Count; i++)
+                {
+                    PlayerData.DamageRessource(gameplayRessourcesNamed[i].ressourceName, _isCharacter, lethalForRessources);
+                }
+            }
         }
 
         void CheckIfGameIsOver()
         {
-
+            
         }
     }
 }
--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -8,6 +8,51 @@ public class PlayerData
     public static List<GameplayRessource> characters;
     public static List<GameplayRessource> tools;
     public static List<GameplayEvent> eventsDone;
+    public static List<GameplayRessource> charactersLost;
+
+    public static void DamageRessource(string _ressourceName, bool _isCharacter, bool _destroy)
+    {
+        GameplayRessource ressource;
+        if (_isCharacter)
+            ressource = characters.Find(x => x.ressourceName == _ressourceName);
+        else
+            ressource = tools.Find(x => x.ressourceName == _ressourceName);
+
+        if (ressource != null)
+        {
+            // Trouver un moyen de communiquer qui est mort ce tour ci 
+            if (ressource.damaged || _destroy)
+            {
+                if (_isCharacter)
+                {
+                    charactersLost.Add(ressource);
+                    characters.Remove(ressource);
+                }
+                else
+                    tools.Remove(ressource);
+            }
+
+            ressource.damaged = true;
+        }
+    }
+
+    public static void ShuffleRessources()
+    {
+        Shuffle(characters);
+        Shuffle(tools);
+    }
+
+    static void Shuffle(List<GameplayRessource> _array)
+    {
+        for (int i = _array.Count - 1; i > 0; i--)
+        {
+            int j = Random.Range(0, i);
+
+            GameplayRessource tmp = _array[i];
+            _array[i] = _array[j];
+            _array[j] = tmp;
+        }
+    }
 
     public static List<GameplayRessource> GetGameplayResourceList(ResourceType resourceType)
     {
