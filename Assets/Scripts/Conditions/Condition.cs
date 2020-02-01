af451b2ea65c9f87a272d40437ac91eccb1559aa
Sébastien Damiens <sebdami@hotmail.fr>
2020-02-01 12:07:03 +0100

    Added Conditions

---
+++ Assets/Scripts/Conditions/Condition.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Condition
+{
+    public bool Invert;
+    public abstract bool Check();
+}
---
+++ Assets/Scripts/Conditions/ConditionList.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConditionList : Condition
+{
+    List<Condition> conditionList = new List<Condition>();
+
+    public enum Required
+    {
+        AllTrueRequired,
+        AnyTrueRequired
+    }
+    public Required required;
+    
+    public override bool Check()
+    {
+        if (conditionList.Count == 0)
+            return true;
+        
+        foreach (var condition in conditionList)
+        {
+            bool result = condition.Check();
+            if (required == Required.AllTrueRequired && !result)
+                return false;
+
+            if (required == Required.AnyTrueRequired)
+                return true;
+        }
+
+        switch (required)
+        {
+            case Required.AllTrueRequired:
+                return true;
+            case Required.AnyTrueRequired:
+                return false;
+            default:
+                return true;
+        }
+    }
+}
