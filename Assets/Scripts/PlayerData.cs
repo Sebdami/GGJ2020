338cdf666a511c3579d8d6f9078100407377c0a4
Antho <transkuja@hotmail.fr>
2020-02-01 11:39:01 +0100

    Create base data classes

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameplayEvent
+public class PlayerData
 {
-
+    public static List<RecruitableCharacter> characters;
 }
--- Assets/Scripts/RecruitableCharacter.cs
+++ Assets/Scripts/RecruitableCharacter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameplayEvent
+public class RecruitableCharacter
 {
-
+    
 }
