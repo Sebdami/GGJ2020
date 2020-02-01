d1227e49d0de774aeead8443e583aca6d513d557
Antho <transkuja@hotmail.fr>
2020-02-01 11:40:56 +0100

    Player data (base)

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -5,4 +5,6 @@ using UnityEngine;
 public class PlayerData
 {
     public static List<RecruitableCharacter> characters;
+    public static List<GameplayTool> tools;
+    public static List<GameplayEvent> eventsDone;
 }
