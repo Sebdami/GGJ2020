b983d3fd3c0589cffb07c430965f490bbbbda422
Sébastien Damiens <sebdami@hotmail.fr>
2020-02-01 14:57:51 +0100

    Scripatble Event

---
+++ Assets/Scripts/ScripatbleObjects/GameplayEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "Data", menuName = "GameplayEvent", order = 1)]
+public class GameplayEvent : ScriptableObject
+{
+    public bool lethal;
+    public bool isEpic;
+    public string eventTitle;
+    public string eventDescription;
+    public GameObject mapPrefab;
+
+    public float timeToResolveEvent; // if < 0.0f, the event is not timed
+
+    public GameplayRessource[] specificTools;
+    public GameplayRessource[] specificCharacters;
+
+    public ConditionList conditionList;
+    public EventChoice[] choices;
+}
