09ee1dcd898f94adae0d563f21a96b4b131835d4
Madmanx <remi.boscher@gmail.com>
2020-02-01 19:31:12 +0100

    Some fixes

--- Assets/Scripts/UI/UIEventChoicesEntry.cs
+++ Assets/Scripts/UI/UIEventChoicesEntry.cs
@@ -12,6 +12,7 @@ public class UIEventChoicesEntry : MonoBehaviour
     public void FillWithInfo(EventChoice e,  UIEvent owner)
     {
         choiceRef = e;
+        content.text = choiceRef.choiceDescription;
         gameObject.SetActive(choiceRef.IsChoiceEnabled());
         GetComponent<Button>().onClick.RemoveAllListeners();
         GetComponent<Button>().onClick.AddListener(() => SelectAChoice());
@@ -22,9 +23,36 @@ public class UIEventChoicesEntry : MonoBehaviour
         string genericFeedbackCosts = ""; // Peut être utilisé sur le panneau de recap
         genericFeedbackCosts = choiceRef.costs.ResolveCosts();
 
-        content.text = choiceRef.choiceDescription;
+  
 
         UIManager.Instance.HidePanel<UIEvent>();
+
+        if(genericFeedbackCosts == string.Empty)
+        {
+            GameplayEventManager GEM = FindObjectOfType<GameplayEventManager>();
+            string nextAction = GEM.choiceMade.ChoiceConsequences();
+            if (string.IsNullOrEmpty(nextAction))
+            {
+                // Close UI
+                // UIManager.Instance.
+                UIManager.Instance.HideAllPanel();
+                // Move
+
+                // Call Trigger event on GEM quand le move est terminé
+            }
+            else
+            {
+                // Close UI
+                UIManager.Instance.HideAllPanel();
+
+                // Call next event
+                GEM.TriggerEvent(nextAction);
+            }
+        }
+        else
+        {
+            UIManager.Instance.ShowPanel<UIRecap>();
+        }
 
         // TODO: Afficher le panneau de recap
         // On peut set un pavé de texte sur le choix pour le recap
---
+++ Assets/Scripts/UI/UIRecap.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class UIRecap : UIPanel
+{
+
+    [Header("Labels")]
+    public TextMeshProUGUI content;
+
+    private GameplayEventManager m_GameplayEventManager;
+
+
+    public override void Show()
+    {
+        base.Show();
+
+        m_GameplayEventManager = FindObjectOfType<GameplayEventManager>();
+        content.text = m_GameplayEventManager.choiceMade.recapAfterChoice;
+ 
+    }
+}
