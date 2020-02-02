dbcfc5d3ae041535e9d12df45af70973c5e3de21
Sébastien Damiens <sebdami@hotmail.fr>
2020-02-02 01:12:28 +0100

    Add audio manager + music

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    [SerializeField]
+    AudioSource musicAudioSource;
+
+    [SerializeField]
+    AnimationCurve fadeCurve;
+
+    [SerializeField]
+    float musicVolume = 1f;
+    
+    public AudioClip mainMusic;
+    public AudioClip endMusic;
+
+    private void Start()
+    {
+        musicAudioSource.clip = mainMusic;
+        musicAudioSource.Play();
+    }
+
+    public void FadeMusic(AudioClip clip, float fadeDuration)
+    {
+        StartCoroutine(MusicFadeCoroutine(clip, fadeDuration));
+    }
+
+    IEnumerator MusicFadeCoroutine(AudioClip clip, float fadeDuration)
+    {
+        float timer = 0.0f;
+        bool changed = false;
+        while(timer < fadeDuration)
+        {
+            float ratio = timer / fadeDuration;
+            musicAudioSource.volume = fadeCurve.Evaluate(ratio) * musicVolume;
+            if(ratio >= .5f && !changed)
+            {
+                musicAudioSource.clip = clip;
+                musicAudioSource.Play();
+                changed = true;
+            }
+            yield return null;
+        }
+        musicAudioSource.volume = musicVolume;
+    }
+}
