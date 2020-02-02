bbcabf2a62c97caedf777b034952c3513ef807b2
Antho <transkuja@hotmail.fr>
2020-02-02 13:37:22 +0100

    Menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,36 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] GameObject credits;
+
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene(1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenCredits()
     {
-
-    }
-    void StartGame()
-    {
-
+        credits.SetActive(!credits.activeInHierarchy);
     }
 
-    void OpenCredits()
+    public void CloseCredits()
     {
-
+        credits.SetActive(false);
     }
 
-    void CloseCredits()
+    public void ExitGame()
     {
-
-    }
-    void ExitGame()
-    {
-
+        Application.Quit();
     }
 }
