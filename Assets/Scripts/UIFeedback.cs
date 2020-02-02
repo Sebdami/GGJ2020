eea61ce737d0b610194aec48e35d76d608541474
Antho <transkuja@hotmail.fr>
2020-02-02 12:16:16 +0100

    WIP ui feedback

---
+++ Assets/Scripts/UIFeedback.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFeedback : MonoBehaviour
+{
+    public float fadeSpeed;
+
+    private void OnEnable()
+    {
+        if (activeFade != null)
+        {
+            StopCoroutine(activeFade);
+        }
+
+        activeFade = StartCoroutine(Fade());
+    }
+
+    Coroutine activeFade = null;
+    IEnumerator Fade()
+    {
+        float lerpParam = 0.0f;
+        while (lerpParam < 1.0f)
+        {
+            lerpParam += Time.deltaTime * fadeSpeed;
+            GetComponent<Text>().color = new Color(GetComponent<Text>().color.r, GetComponent<Text>().color.g, GetComponent<Text>().color.b, 1 - lerpParam);
+            yield return new WaitForEndOfFrame();
+        }
+
+        activeFade = null;
+    }
+}
