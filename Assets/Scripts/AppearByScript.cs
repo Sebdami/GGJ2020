50370c65a5359f8b2d5d81ab9de0dbd282898727
Sébastien Damiens <sebdami@hotmail.fr>
2020-02-05 12:31:11 +0100

    WINDMILL

---
+++ Assets/Scripts/AppearByScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearByScript : MonoBehaviour
+{
+    [SerializeField]
+    Transform toScale;
+
+    [SerializeField]
+    AnimationCurve appearAnimation;
+
+    [SerializeField]
+    float animationDuration;
+
+    public void Start()
+    {
+        toScale.localScale = Vector3.zero;
+    }
+
+    public void Appear()
+    {
+        StartCoroutine(AppearCoroutine());
+    }
+    IEnumerator AppearCoroutine()
+    {
+        float timer = 0f;
+        while(timer < animationDuration)
+        {
+            timer += Time.deltaTime;
+            toScale.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, appearAnimation.Evaluate(timer / animationDuration));
+            yield return null;
+        }
+        toScale.localScale = Vector3.one;
+    }
+}
